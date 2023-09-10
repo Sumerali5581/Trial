0ec98e908a71b48673daaf3562e73b2a47e8fdad
Sumer Ali <sumersmr7@gmail.com>
2023-09-10 10:47:24 +0530

    testing

--- hello.java
+++ hello.java
@@ -3,5 +3,7 @@ class a
     public static void main(String ar[])
     {
         System.out.println("Hello");
+        System.out.println("Pune");
+        System.out.println("We want to test");
     }
 }
